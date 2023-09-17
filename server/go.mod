ac4cb27d6e76d0a2ad43b52fd6cd9d496f61252b
Reid Mason <maddogshain132@gmail.com>
2023-09-17 20:40:02 +0100

    Fixed bug with migration error handling

--- server/go.mod
+++ server/go.mod
@@ -5,11 +5,11 @@ go 1.21.1
 require (
 	github.com/go-chi/chi v1.5.5
 	github.com/golang-migrate/migrate/v4 v4.16.2
+	github.com/google/uuid v1.3.1
 	github.com/lib/pq v1.10.9
 )
 
 require (
-	github.com/google/uuid v1.3.1 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
--- server/internal/migrations/migrations.go
+++ server/internal/migrations/migrations.go
@@ -34,12 +34,12 @@ func Migrate(db *sql.DB) {
 	migrationInstance := getDatabaseMigrationInstance(db)
 	slog.Info("Performing database migration")
 	err := migrationInstance.Up()
-	if err != nil && err.Error() != migrate.ErrNoChange.Error() {
-		slog.Error("Failed to migrate", "error", err)
-		log.Panic()
+	if err == nil {
+		slog.Info("Database migration complete")
 	} else if err.Error() == migrate.ErrNoChange.Error() {
 		slog.Info("Database migration complete - No changes")
 	} else {
-		slog.Info("Database migration complete")
+		slog.Error("Failed to migrate", "error", err)
+		log.Panic("Failed to migrate")
 	}
 }
